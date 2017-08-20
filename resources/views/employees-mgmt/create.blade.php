836bb0617fcebced5765e7c8adcd8a8a305d2ccd
prakashperumal <prakash.perumal@latentview.com>
2017-08-20 10:24:59 +0530

    Bugs fixed

--- resources/views/employees-mgmt/create.blade.php
+++ resources/views/employees-mgmt/create.blade.php
@@ -153,7 +153,7 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Birthday</label>
+                            <label class="col-md-4 control-label">Birthdate</label>
                             <div class="col-md-6">
                                 <div class="input-group date">
                                     <div class="input-group-addon">

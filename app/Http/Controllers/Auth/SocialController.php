f9200f3faeb143a985b5ca3cd7d8ad9281bb184f
prakashperumal <prakash.perumal@latentview.com>
2017-10-01 07:34:22 +0530

    #issue fixed

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -77,6 +77,7 @@ class SocialController extends Controller
                 'picture' =>$user->avatar,
                 'password' => bcrypt($email),
             ]);
+           return response('Unauthorized.', 401);
         }
 
         if (Auth::attempt(['email' => $email, 'password' => $email])){
